6e10bb062771da2548c2a9b295290e3be4610421
Fetmouche Elyes <elyes.fetmouche@etu.univ-orleans.fr>
2025-11-04 18:26:35 +0100

    Question 3 (Elyes) : création du modèle Consultation

--- medico/models.py
+++ medico/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Consultation(models.Model):
+    patient_nom = models.CharField(max_length=40, null=False)
+    patient_prenom = models.CharField(max_length=30, null=False)
+    patient_genre = models.CharField(
+    max_length=10,
+    choices=[('M', 'Homme'), ('F', 'Femme')],
+    null=False
+    )
+    patient_age = models.PositiveIntegerField(null=False)
+    description = models.TextField(null=False)
+    date_consultation = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.patient_prenom} {self.patient_nom} - {self.date_consultation}"
