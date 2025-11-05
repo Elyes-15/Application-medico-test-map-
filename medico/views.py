2bf63f234f1c9ec37db349ba8a1656e3a115f662
Rayan Touak <rayantouak6@gmail.com>
2025-11-05 19:08:23 +0100

    Q8: Suppression d'une consultation avec confirmation

--- medico/views.py
+++ medico/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Consultation
 
 def about(request):
@@ -12,3 +12,10 @@ def consultation_list(request):
     consultations = Consultation.objects.all()
     return render(request, 'medico/liste_consultation.html', {'consultations': consultations})
 
+def effacer_consultation(request, consultation_id):
+    consultation = get_object_or_404(Consultation, pk=consultation_id)
+
+    if request.method == 'POST':
+        consultation.delete()
+        return redirect('liste_consultation')
+    return render(request, 'medico/effacer_consultation.html', {'consultation': consultation})
