0fe3cef1a1fd90732ffe955ffa0ba767244c5cd5
Fetmouche Elyes <elyes.fetmouche@etu.univ-orleans.fr>
2025-11-05 20:10:53 +0100

    Mise à jour urls.py pour question 7

--- medico/urls.py
+++ medico/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('consultation/<int:n>/', views.details_consultation, name='details_consultation'),
     path('consultations/', views.consultation_list, name='liste_consultaiton'),
+    
 ]
